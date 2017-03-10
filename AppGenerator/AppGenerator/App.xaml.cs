96ed431b387d2da06681ac5a0f6ade883fc1e43d
Ravi Ranjan <rvrn22@gmail.com>
2017-03-10 16:15:29 -0600

    Initial project template.

---
+++ AppGenerator/AppGenerator/App.xaml.cs
@@ -0,0 +1,8 @@
+﻿using System.Windows;
+
+namespace AppGenerator
+{
+    public partial class App : Application
+    {
+    }
+}
---
+++ AppGenerator/AppGenerator/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace AppGenerator
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
